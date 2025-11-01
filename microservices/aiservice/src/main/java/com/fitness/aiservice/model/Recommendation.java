fc581012e00cac02c160f92cc6c4b8cedab158a6
Aman Maurya <amanmaurya.2709@gmail.com>
2025-11-02 03:31:46 +0530

    Stored the AI Response to Database

--- microservices/aiservice/src/main/java/com/fitness/aiservice/model/Recommendation.java
+++ microservices/aiservice/src/main/java/com/fitness/aiservice/model/Recommendation.java
@@ -22,6 +22,7 @@ public class Recommendation {
     private String activityId;
     private String userId;
     private String activityType;
+    private String recommendation;
     private List<String> improvements;
     private List<String> suggestions;
     private List<String> safety;
